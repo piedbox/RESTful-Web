0805a73324c8238cbf7519e441bcc81a7871e175
fallenangeldark <piedbox@gmail.com>
2019-03-06 22:42:36 +1000

    Last commit for toys Model. Next -> People

--- toys/views.py
+++ toys/views.py
@@ -1,56 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from rest_framework import status
 from toys.models import Toy
 from toys.serializers import ToySerializer
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+
+# decorator api_view generates the appropriate response for the unsupported HTTP verbs or methods This decorator is a wrapper that converts a function-based view into a subclass of the  rest_framework.views.APIView class
+# Теперь сможем заюзать http OPTIONS 0.0.0.0:8000/toys/2/
+# для определения параметров или требований, связанных с ресурсом
+# http 0.0.0.0:8000/toys/ Accept:text/html - даст ответ в html разметке
 
 
-@csrf_exempt  # Отключаем проверку csrf
+@api_view(['GET', 'POST'])
 def toy_list(request):
+    """Для получения списка элементов. """
     if request.method == 'GET':
         toys = Toy.objects.all()
-        # many=True сериализация списка объектов
         toys_serializer = ToySerializer(toys, many=True)
-        return JSONResponse(toys_serializer.data)
+        return Response(toys_serializer.data)
+
     elif request.method == 'POST':
-        toy_data = JSONParser().parse(request)
-        toy_serializer = ToySerializer(data=toy_data)
+        toy_serializer = ToySerializer(data=request.data)
         if toy_serializer.is_valid():
             toy_serializer.save()
-            return JSONResponse(toy_serializer.data, \
-                status=status.HTTP_201_CREATED)
-        return JSONResponse(toy_serializer.errors, \
-            status=status.HTTP_400_BAD_REQUEST)
+            return Response(toy_serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(toy_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def toy_detail(request, pk):
+    """Для получения/изменения/отправки одного элемента."""
     try:
         toy = Toy.objects.get(pk=pk)
     except Toy.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
         toy_serializer = ToySerializer(toy)
-        return JSONResponse(toy_serializer.data)
+        return Response(toy_serializer.data)
+
     elif request.method == 'PUT':
-        toy_data = JSONParser().parse(request)
-        toy_serializer = ToySerializer(toy, data=toy_data)
+        toy_serializer = ToySerializer(toy, data=request.data)
         if toy_serializer.is_valid():
             toy_serializer.save()
-            return JSONResponse(toy_serializer.data)
-        return JSONResponse(toy_serializer.errors, \
-            status=status.HTTP_400_BAD_REQUEST)
+            return Response(toy_serializer.data)
+        return Response(toy_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         toy.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
